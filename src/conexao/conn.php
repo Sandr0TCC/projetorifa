7ed147f78c7aba89f03e417bfd9a61e1fed5b30a
SandroSouzaJr <sandro.2005.02.11@gmail.com>
2022-06-04 16:44:35 -0300

    commit

--- src/conexao/conn.php
+++ src/conexao/conn.php
@@ -6,3 +6,13 @@ $database = "epiz_31858958_rifaorigins";
 $user = "epiz_31858958";
 $password = "66foh902bz";
 
+try{
+    $pdo = new PDO('mysql:host='.$hostname.';dbname='.$database, $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo 'Conexão com o banco de dados '.$database.', foi realizado com sucesso!';
+
+}catch (PDOException $e) {
+    echo 'Erro: '.$e->getMessage();
+
+}
+
