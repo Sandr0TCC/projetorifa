cdb91b11125e482f0af79694d15f45ff765406b8
SandroSouzaJr <sandro.2005.02.11@gmail.com>
2022-06-26 17:49:40 -0300

    commit

---
+++ src/tipo/controle/delete-tipo.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+    $('#table-tipo').on('click', 'button.btn-delete', function(e) {
+        e.preventDefault()
+
+        let ID = `ID=${$(this).attr('id')}`
+
+        Swal.fire({
+            title: 'SysRifa',
+            text: 'Deseja realmente excluir este registro?',
+            icon: 'question',
+            showCancelButton: true,
+            confirmButtonText: 'SIM',
+            cancelButtonText: 'NÃO'
+        }).then((result) => {
+            if (result.value) {
+                $.ajax({
+                    type: 'POST',
+                    dataType: 'json',
+                    assync: true,
+                    data: ID,
+                    url: 'src/tipo/modelo/delete-tipo.php',
+                    success: function(dados) {
+                        Swal.fire({
+                            title: 'SysRifa',
+                            text: dados.mensagem,
+                            icon: dados.tipo,
+                            confirmButtonText: 'OK'
+                        })
+                    }
+                })
+            }
+        })
+    })
+})
